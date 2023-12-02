871f746528b58aab412ce1f7735ba396ee400c78
krishnamurthypranesh <pk96ishere@gmail.com>
2023-12-02 17:51:19 +0000

    Implement list collections

--- backend/journal/views.py
+++ backend/journal/views.py
@@ -1,16 +1,36 @@
 import json
 import logging
+from typing import Optional
 
 import constants
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
-from exc import ConflictingCollectionName, InvalidCollectionTemplate, ObjectNotFound
+from exc import (
+    BadPaginationParameter,
+    ConflictingCollectionName,
+    InvalidCollectionTemplate,
+    ObjectNotFound,
+)
 from helpers import generate_id
 from journal.models import Collection
-from journal.schema import CollectionTemplate
+from journal.schema import CollectionOut, CollectionTemplate, ListCollectionResponse
 
 logger = logging.getLogger("module::journal")
+
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def collections_dispatcher(request, *args, **kwargs):
+    if request.method.upper() == "GET":
+        params = {
+            k: request.GET.get(k, None)
+            for k in ["limit", "starting_after", "ending_before", "status"]
+        }
+        return list_collections(request, **params)
+
+    if request.method.upper() == "POST":
+        return create_collection(request, *args, **kwargs)
 
 
 @login_required
@@ -55,7 +75,7 @@ def create_collection(request):
         "name": collection.name,
         "template": collection.template,
         "active": collection.active,
-        "created_at": collection.created_at.isoformat(),
+        "created_at": collection.created_at.replace(microsecond=0).isoformat(),
     }
 
     return JsonResponse(data=resp, status=201)
@@ -77,73 +97,65 @@ def get_collection(request, collection_id):
         "name": collection.name,
         "template": collection.template,
         "active": collection.active,
-        "created_at": collection.created_at.isoformat(),
+        "created_at": collection.created_at.replace(microsecond=0).isoformat(),
     }
 
     return JsonResponse(data=resp, status=200)
 
 
-# def list_items(
-#     self,
-#     next_cursor: Optional[str] = None,
-#     prev_cursor: Optional[str] = None,
-#     limit: int = 20,
-#     auth_token = Depends(authorize),
-# ) -> ListCollectionResponse:
-#     # for now, the list api will just accept the limit and next_cursor parameters
-#     if limit > constants.MAX_PAGINATION_LIMIT:
-#         limit = constants.MAX_PAGINATION_LIMIT
+@login_required
+@require_http_methods(["GET"])
+def list_collections(
+    request,
+    starting_after: Optional[str] = None,
+    ending_before: Optional[str] = None,
+    limit: int = None,
+    status: int = None,
+) -> ListCollectionResponse:
+    # for now, the list api will just accept the limit and next_cursor parameters
+    limit = int(limit)
+    if limit > constants.MAX_PAGINATION_LIMIT:
+        limit = constants.MAX_PAGINATION_LIMIT
 
-#     conditions = [
-#         next_cursor is not None,
-#         prev_cursor is not None,
-#     ]
+    conditions = [
+        starting_after is not None,
+        ending_before is not None,
+    ]
 
-#     if all(conditions):
-#         raise BadPaginationParameter()
+    if all(conditions):
+        raise BadPaginationParameter(
+            "One of (starting_after, ending_before) must be supplied"
+        )
 
-#     scan_forward = True
-#     cursor = ""
-#     if next_cursor is not None:
-#         cursor = next_cursor
+    query = Collection.objects.filter(user_id=request.user.id).order_by("-gid")
 
-#     if prev_cursor is not None:
-#         cursor = prev_cursor
-#         scan_forward = False
+    if starting_after is not None:
+        query = query.filter(gid__gt=starting_after)
 
-#     # the response will be a list of collections of the given size (provided the list is less than 1MB in size)
-#     records, key = self.painted_porch_repo.get_all_collections_by_params(
-#         user_id=auth_token.user.user_id,
-#         cursor=cursor,
-#         scan_forward=scan_forward,
-#         limit=limit,
-#     )
+    if ending_before is not None:
+        query = query.filter(gid__lt=ending_before)
 
-#     ret_val = []
-#     for rec in records:
-#         ret_val.append(
-#             CollectionOut(
-#                 collection_id=rec.collection_id,
-#                 name=rec.collection_id,
-#                 template=rec.template,
-#                 active=rec.active,
-#                 created_at=rec.created_at.isoformat(),
-#             )
-#         )
+    # the response will be a list of collections of the given size (provided the list is less than 1MB in size)
+    records = query[:limit]
 
-#     if scan_forward:
-#         prev_cursor = next_cursor
-#         next_cursor = key
-#     else:
-#         next_cursor = prev_cursor
-#         prev_cursor = key
+    ret_val = []
+    for rec in records:
+        ret_val.append(
+            CollectionOut(
+                collection_id=rec.gid,
+                name=rec.name,
+                template=rec.template,
+                active=rec.active,
+                created_at=rec.created_at.replace(microsecond=0).isoformat(),
+            )
+        )
 
-#     return ListCollectionResponse(
-#         next_cursor=next_cursor,
-#         prev_cursor=prev_cursor,
-#         limit=limit,
-#         records=ret_val,
-#     )
+    response = ListCollectionResponse(
+        limit=limit,
+        records=ret_val,
+    )
+
+    return JsonResponse(data=response.model_dump(), status=200)
 
 
 def patch(request):
