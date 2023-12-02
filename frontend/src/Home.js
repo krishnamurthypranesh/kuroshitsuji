5f7bcd435c4c82cc79e0337ff30259548530c973
krishnamurthypranesh <pk96ishere@gmail.com>
2023-12-02 18:15:41 +0000

    Add column: collection id to table

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -15,50 +15,29 @@ const Home = (props) => {
     }
 
     async function listCollections() {
-        // const userToken = JSON.parse(localStorage.getItem("user")).token;
+        const userToken = JSON.parse(localStorage.getItem("user")).token;
 
-        // const response = await fetch(
-        //     "http://localhost:8000/v1/journal/collections",
-        //     {
-        //         method: "GET",
-        //         headers: {
-        //             "Authorization": "Bearer " + userToken,
-        //             "Content-Type": "application/json",
-        //         }
-        //     }
-        // );
+        const response = await fetch(
+            "http://localhost:8000/v1/journal/collections/?limit=10",
+            {
+                method: "GET",
+                headers: {
+                    "Authorization": "Bearer " + userToken,
+                    "Content-Type": "application/json",
+                }
+            }
+        );
 
-        // if (!response.ok) {
-        //     console.log("error fetching data from server");
-        //     throw new Error("error fetching data from server");
-        // }
+        if (!response.ok) {
+            console.log("error fetching data from server");
+            throw new Error("error fetching data from server");
+        }
 
-        // const data = await response.json();
+        const data = await response.json();
 
-        // setCollectionsList(data)
-
-        setCollectionsList({
-            records: [
-                {
-                    "name": "Test 1",
-                    "created_at": "2023-12-02 01:00:00",
-                },
-                {
-                    "name": "Test 2",
-                    "created_at": "2023-12-02 02:00:00",
-                },
-                {
-                    "name": "Test 3",
-                    "created_at": "2023-12-02 03:00:00",
-                },
-            ]
-        })
+        setCollectionsList(data)
     }
 
-    // the layout of this page is pretty simple
-    // load the collections table
-    // show a button above the collections table showing a place for adding new collections
-    // show a table listing all collections for this user account
     const renderTable = () => {
         const data = collectionsList;
 
@@ -66,6 +45,7 @@ const Home = (props) => {
             return (
                 <tr>
                     <td>{index + 1}</td>
+                    <td>{record.collection_id}</td>
                     <td>{record.name}</td>
                     <td>{record.created_at}</td>
                 </tr>
@@ -81,6 +61,7 @@ const Home = (props) => {
                         <thead>
                             <tr>
                             <th scope="col">Sl No</th>
+                            <th scope="col">Collection ID</th>
                             <th scope="col">Name</th>
                             <th scope="col">Created At</th>
                             </tr>
